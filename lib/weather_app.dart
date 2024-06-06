d1d818d616c725a3c9710a2036cf9a528d97855b
Pawan Kumar <pk89844@gmail.com>
2024-06-06 11:17:05 +0530

    Project Starter

---
+++ lib/weather_app.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:weather/module/home/view/home_view.dart';
+
+class WeatherApp extends StatelessWidget {
+  const WeatherApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Weather',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomeView(),
+    );
+  }
+}
